35c6a62fc8c3e9c4d1019cee573b84d0b2a86e01
machard <pascal.machard@gmail.com>
2024-02-16 14:48:09 +0100

    Update Coord3D.cs refactoring

--- Coord3D.cs
+++ Coord3D.cs
@@ -7,72 +7,42 @@ class Coord3D : ICoord3d
     
     public Coord3D(double x, double y , double z)
     {
-        X = x;
-        Y = y;
-        Z = z;
+        X = x; Y = y; Z = z;
     }
     
     public ICoord3D operator_add(ICoord3D b)
     {
-        return new Coord3D (
-            this.X + b.X,
-            this.Y + b.Y,
-            this.Z + b.Z
-        );
+        return new Coord3D ( this.X + b.X, this.Y + b.Y, this.Z + b.Z );
     }
     
     static public ICoord3D operator_supp(ICoord3D a)
     {
-        return new Coord3D (
-            this.X - b.X,
-            this.Y - b.Y,
-            this.Z - b.Z
-        );
+        return new Coord3D ( this.X - b.X, this.Y - b.Y, this.Z - b.Z );
     }
     
     public ICoord3D operator_supp(ICoord3D b)
     {
-        return new Coord3D (
-            - b.X,
-            - b.Y,
-            - b.Z
-        );
+        return new Coord3D ( - b.X, - b.Y, - b.Z);
     }
     
     public ICoord3D operator_mult(ICoord3D b)
     {
-        return new Coord3D (
-            this.X * b.X,
-            this.Y * b.Y,
-            this.Z * b.Z
-        );
+        return new Coord3D ( this.X * b.X, this.Y * b.Y, this.Z * b.Z);
     }
 
     public ICoord3D operator_mult(double b)
     {
-        return new Coord3D (
-            this.X * b,
-            this.Y * b,
-            this.Z * b
-        );
+        return new Coord3D ( this.X * b, this.Y * b, this.Z * b);
     }
 
     public ICoord3D operator_div(ICoord3D b)
     {
-        return new Coord3D (
-            this.X / b.X,
-            this.Y / b.Y,
-            this.Z / b.Z
-        );    
+        return new Coord3D ( this.X / b.X, this.Y / b.Y, this.Z / b.Z);    
     }
     
     public ICoord3D operator_div(double b)
     {
-        return new Coord3D (
-            this.X / b,
-            this.Y / b,
-            this.Z / b
-        );    
+        return new Coord3D ( this.X / b, this.Y / b, this.Z / b);    
     }
 
     public double operator_scal(ICoord3D b)
@@ -82,11 +52,6 @@ class Coord3D : ICoord3d
 
     public ICoord3d operator_produit_vect(ICoord3D b)
     {
-        u2v3−u3v2,u3v1−u1v3,u1v2−u2v1
-        return new Coord3D (
-            this.Y*b.Z - this.Z*b.Y,
-            this.Z*b.X - this.X*b.Z,
-            this.X*b.Y - this.Y*b.X
-        );    
+        return new Coord3D ( this.Y*b.Z - this.Z*b.Y, this.Z*b.X - this.X*b.Z, this.X*b.Y - this.Y*b.X );    
     }
 };
