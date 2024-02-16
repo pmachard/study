a3a309701d5ad8ff60f772bacb72386d7d17c7d1
machard <pascal.machard@gmail.com>
2024-02-16 15:23:54 +0100

    Update SetEdge.cs Ajout d'un point

--- SetEdge.cs
+++ SetEdge.cs
@@ -1,13 +1,15 @@
 class SetEdge : ISetEdge
 {
+    private List<ICoord3D> Coords;
+    
     public SetEdge()
     {
-
+        Coords = new List<ICoord3D>();
     }
 
     public AddPoint(ICoord Pt)
     {
-
+        Coords.Add(Pt);
     }
   
 };
