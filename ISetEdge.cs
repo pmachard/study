452cf107f70a13a9ee5590cafad1fd3d0c0ceb35
machard <pascal.machard@gmail.com>
2024-02-16 15:48:50 +0100

    Update ISetEdge.cs

--- ISetEdge.cs
+++ ISetEdge.cs
@@ -1,8 +1,10 @@
 interface ISetEdge : ITranslatable
 {
+    // Ensemble de point par default vide 
     public ISetEdge();
-
+    // Ajout d'un point dans la liste des point
     public AddPoint(ICoord Pt);
-    
+
+    // Iterator sur les points
     public IEnumerator<ICoord3D> GetCoord();
 };
