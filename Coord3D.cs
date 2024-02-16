8ca17f5652d08e5ae44771c7bc1746603e77459c
machard <pascal.machard@gmail.com>
2024-02-16 14:20:33 +0100

    Update Coord3D.cs creataion des entetes

--- Coord3D.cs
+++ Coord3D.cs
@@ -1,1 +1,33 @@
+class Coord3D : ICoord3d
+{
+    public Coord3D()
+    {
+    }
+    public Coord3D(double x, double y , double z)
+    {
+    }
+    
+    public ICoord3D operator_add(ICoord3D b)
+    {
+    }
+    
+    static public ICoord3D operator_supp(ICoord3D a)
+    {
+    }
+    
+    public ICoord3D operator_supp(ICoord3D b)
+    {
+    }
+    
+    public ICoord3D operator_mult(ICoord3D b)
+    {
+    }
 
+    public ICoord3D operator_mult(double b)
+    {
+    }
+
+    public ICoord3D operator_div(ICoord3D b)
+    {
+    }
+};
