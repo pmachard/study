212d285fc9aaef1205c4c4eb58173c034d54525d
machard <pascal.machard@gmail.com>
2024-02-16 16:36:16 +0100

    Update ICube.cs

--- ICube.cs
+++ ICube.cs
@@ -3,6 +3,5 @@ interface ICube : IObject3D
   public ICube();
   public ICube(ICoordSystem coordSystem, double size);
 
-  public ICoordSystem CoSystem {get; set;}
   public double Size(get;set);
 }
