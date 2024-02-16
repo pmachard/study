178ed388100fc52b364832f7edb831abc3be51d0
machard <pascal.machard@gmail.com>
2024-02-16 15:05:51 +0100

    Create CoordSystem.cs creation

---
+++ CoordSystem.cs
@@ -0,0 +1,25 @@
+class CoordSystem : ICoordSystem
+{
+    public Coord3D()
+    {
+      O = new Coord3D(0.0,0.0,0.0);
+      I = new Coord3D(1.0,0.0,0.0);
+      J = new Coord3D(0.0,1.0,0.0);
+      K = new Coord3D(0.0,0.0,1.0);
+    }
+
+    public ICoord3D(ICoord3D o,ICoord3D i,ICoord3D j,ICoord3D k)
+    {
+      O = new Coord3D(o);
+      I = new Coord3D(i);
+      J = new Coord3D(j);
+      K = new Coord3D(k);
+    }
+  
+    public ICoord3d O {get; set;};
+    public ICoord3d I {get; set;};
+    public ICoord3d J {get; set;};
+    public ICoord3d K {get; set;};
+
+  
+}
