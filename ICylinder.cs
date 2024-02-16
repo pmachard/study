1b2ff38802139eecab07b2e7a86ae3bce20e1e25
machard <pascal.machard@gmail.com>
2024-02-16 17:06:21 +0100

    Update ICylinder.cs

--- ICylinder.cs
+++ ICylinder.cs
@@ -1,10 +1,15 @@
 interface ICylinder : IObject3D
 {
+  // constructeur par default du cylindre 1.0 * 1.0
   public ICylinder();
+  // constructeur avec le repère local et le rayon et la hauteur
   public ICylinder(ICoordSystem localCoordSystem, double radius, double height);
-  
+
+  // rayon du cylindre
   public double Radius {get;set;}
+  // hauteur du cylindre
   public double Height {get;set;}
+  // repere du cylindre
   public ICoordSystem LocalCoordSystem {get; set;};
 
 }
