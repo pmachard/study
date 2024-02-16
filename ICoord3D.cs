504b126c78eba2e8c74130e3dccf8896df1c22c5
machard <pascal.machard@gmail.com>
2024-02-16 14:27:56 +0100

    Update ICoord3D.cs ajout produit vectoriel et produit scalaire

--- ICoord3D.cs
+++ ICoord3D.cs
@@ -13,6 +13,8 @@ interface ICoord3d
     static public ICoord3 operator * (ICoord3D a, ICoord3D b) { return a.operator_mult(b); }
     static public ICoord3 operator * (double m) { return a.operator_mult(m); }
     static public ICoord3 operator / (ICoord3D a, ICoord3D b) { return a.operator_div(b); }
+    static public double operator ^ (ICoord3D a, ICoord3D b) { return a.operator_scal(b); }
+    static public ICoord3 operator & (ICoord3D a, ICoord3D b) { return a.operator_produit_vect(b); }
 
     public ICoord3D operator_add(ICoord3D b);
     static public ICoord3D operator_supp(ICoord3D a);
@@ -21,4 +23,6 @@ interface ICoord3d
     public ICoord3D operator_mult(double b);
     public ICoord3D operator_div(ICoord3D b);
     public ICoord3D operator_div(double b);
+    public double operator_scal(ICoord3D b);
+    public ICoord3D operator_produit_vect(ICoord3D b);
 }
