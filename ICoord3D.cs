bd5b26e4202b950df4722a1eed4f6bad57997acf
machard <pascal.machard@gmail.com>
2024-02-16 14:09:39 +0100

    Update ICoord3D.cs ajout d'operator

--- ICoord3D.cs
+++ ICoord3D.cs
@@ -8,13 +8,16 @@ interface ICoord3d
     public double Z { get; set; }
 
     static public ICoord3 operator + (ICoord3D a, ICoord3D b) { return a.operator_add(b); }
+    static public ICoord3 operator - (ICoord3D a) { return operator_supp(a); }
     static public ICoord3 operator - (ICoord3D a, ICoord3D b) { return a.operator_supp(b); }
     static public ICoord3 operator * (ICoord3D a, ICoord3D b) { return a.operator_mult(b); }
+    static public ICoord3 operator * (double m) { return a.operator_mult(m); }
     static public ICoord3 operator / (ICoord3D a, ICoord3D b) { return a.operator_div(b); }
 
     public operator_add(ICoord3D b);
+    static public operator_supp(ICoord3D a);
     public operator_supp(ICoord3D b);
     public operator_mult(ICoord3D b);
+    public operator_mult(double b);
     public operator_div(ICoord3D b);
-    
 }
