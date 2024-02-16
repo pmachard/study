3675f2fc93b1edbb6c840a56d560c0d69ad4510e
machard <pascal.machard@gmail.com>
2024-02-16 14:24:25 +0100

    Update Coord3D.cs corps de methode de base

--- Coord3D.cs
+++ Coord3D.cs
@@ -2,32 +2,76 @@ class Coord3D : ICoord3d
 {
     public Coord3D()
     {
+        X = Y = Z = 0.0;
     }
+    
     public Coord3D(double x, double y , double z)
     {
+        X = x;
+        Y = y;
+        Z = z;
     }
     
     public ICoord3D operator_add(ICoord3D b)
     {
+        return new Coord3D (
+            this.X + b.X,
+            this.Y + b.Y,
+            this.Z + b.Z
+        );
     }
     
     static public ICoord3D operator_supp(ICoord3D a)
     {
+        return new Coord3D (
+            this.X - b.X,
+            this.Y - b.Y,
+            this.Z - b.Z
+        );
     }
     
     public ICoord3D operator_supp(ICoord3D b)
     {
+        return new Coord3D (
+            - b.X,
+            - b.Y,
+            - b.Z
+        );
     }
     
     public ICoord3D operator_mult(ICoord3D b)
     {
+        return new Coord3D (
+            this.X * b.X,
+            this.Y * b.Y,
+            this.Z * b.Z
+        );
     }
 
     public ICoord3D operator_mult(double b)
     {
+        return new Coord3D (
+            this.X * b,
+            this.Y * b,
+            this.Z * b
+        );
     }
 
     public ICoord3D operator_div(ICoord3D b)
     {
+        return new Coord3D (
+            this.X / b.X,
+            this.Y / b.Y,
+            this.Z / b.Z
+        );    
+    }
+    
+    public ICoord3D operator_div(double b)
+    {
+        return new Coord3D (
+            this.X / b,
+            this.Y / b,
+            this.Z / b
+        );    
     }
 };
