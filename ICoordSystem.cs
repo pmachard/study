a3d23df7ac831e86c3f68e119ef6a96a65e203a4
machard <pascal.machard@gmail.com>
2024-02-16 15:00:38 +0100

    Create ICoordSystem.cs création

---
+++ ICoordSystem.cs
@@ -0,0 +1,11 @@
+interface ICoordSystem : ICoord3D
+{
+    public ICoord3D();
+    public ICoord3D(ICoord3D o,ICoord3D i,ICoord3D j,ICoord3D k);
+  
+    public ICoord3d I {get; set;};
+    public ICoord3d J {get; set;};
+    public ICoord3d K {get; set;};
+
+  
+}
