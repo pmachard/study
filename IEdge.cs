ed5fe6fa82b32bf9a7bae002b964bc409b15e335
machard <pascal.machard@gmail.com>
2024-02-16 15:38:14 +0100

    Update IEdge.cs

--- IEdge.cs
+++ IEdge.cs
@@ -1,8 +1,14 @@
-interface IEdge
+interface IEdge : ITranslatable 
 {
   public IEdge ();
   public IEdge (ICoord3D a, ICoord3D b);
   
   public ICoord3D A( get; set;);
   public ICoord3D B( get; set;);
+
+    public RunTranlation(ICoord3D v)
+    {
+        A = A + v;
+        B = B + v;
+    }
 }
