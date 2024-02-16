fdf161f9fee5a17232ba4d3fc1df07f022ee3d32
machard <pascal.machard@gmail.com>
2024-02-16 15:15:13 +0100

    Create Edge.cs création

---
+++ Edge.cs
@@ -0,0 +1,15 @@
+class Edge : IEdge
+{
+  public Edge ()
+  {
+    A = B = new Coord3D()
+  }
+  public IEdge (ICoord3D a, ICoord3D b)
+  {
+    A = a;
+    B = b;
+  }
+  
+  public ICoord3D A( get; set;);
+  public ICoord3D B( get; set;);
+}
