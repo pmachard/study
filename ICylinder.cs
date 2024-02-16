b34a8ca23682799e58fbfb62670c16bf0f1f97c4
machard <pascal.machard@gmail.com>
2024-02-16 17:04:43 +0100

    Update ICylinder.cs

--- ICylinder.cs
+++ ICylinder.cs
@@ -1,17 +1,7 @@
-class Cylinder : ICylinder 
+interface ICylinder : IObject3D
 {
-  public Cylinder()
-  {
-    Radius = 1.0;
-    Height = 1.0;
-    LocalCoordSystem = new ICoordSytem();
-  }
-  public ICylinder(ICoordSystem localCoordSystem, double radius, double height)
-  {
-    Radius = radius;
-    Height = height;
-    LocalCoordSystem = localCoordSystem;
-  }
+  public ICylinder();
+  public ICylinder(ICoordSystem localCoordSystem, double radius, double height);
   
   public double Radius {get;set;}
   public double Height {get;set;}
