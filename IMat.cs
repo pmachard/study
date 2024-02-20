9cefe2164844c4e4c6d1dda84c194b332aae7553
machard <pascal.machard@gmail.com>
2024-02-20 18:05:06 +0100

    Update IMat.cs

--- IMat.cs
+++ IMat.cs
@@ -6,40 +6,31 @@ interface IMat
     public double get(int n,int m); 
 
     // Operateur addition entre deux matrice
-    static public IMat operator + (IMat a, ICoord3D b) { return a.operator_add(b); }
-    // Operateur d'inversion entre deux coordonnées
-    static public ICoord3 operator - (ICoord3D a) { return operator_supp(a); }
-    // Operateur d'affection entre deux coordonnées
-    static public ICoord3 operator - (ICoord3D a, ICoord3D b) { return a.operator_supp(b); }
-    // Operateur de multiplication entre deux coordonnées
-    static public ICoord3 operator * (ICoord3D a, ICoord3D b) { return a.operator_mult(b); }
-    // Operateur de multiplication entre des coordonnées et un double
+    static public IMat operator + (IMat a, IMat b) { return a.operator_add(b); }
+    // Operateur d'inversion entre deux matrice
+    static public IMat operator - (IMat a) { return operator_supp(a); }
+    // Operateur d'affection entre deux matrices
+    static public IMat operator - (IMat a, IMat b) { return a.operator_supp(b); }
+    // Operateur de multiplication entre deux matrices
+    static public IMat operator * (IMat a, IMat b) { return a.operator_mult(b); }
+    // Operateur de multiplication entre une matrice et un double
     static public ICoord3 operator * (double m) { return a.operator_mult(m); }
-    // Operateur de division entre deux coordonnées
-    static public ICoord3 operator / (ICoord3D a, ICoord3D b) { return a.operator_div(b); }
-    // Operateur produit scalaire entre deux coordonnées
-    static public double operator ^ (ICoord3D a, ICoord3D b) { return a.operator_scal(b); }
-    // Operator produit vectoriel entre deux coordonnées
-    static public ICoord3 operator & (ICoord3D a, ICoord3D b) { return a.operator_produit_vect(b); }
+    // Operateur de division entre deux matrices
+    static public IMat operator / (IMat a, IMat b) { return a.operator_div(b); }
 
-    // Ajout de coodonnées
-    public ICoord3D operator_add(ICoord3D b);
-    // Inversion de coodonnées
-    static public ICoord3D operator_supp(ICoord3D a);
-    // Difference entre deux coordonnées
-    public ICoord3D operator_supp(ICoord3D b);
-    // Multiplication entre deux coordonnées
-    public ICoord3D operator_mult(ICoord3D b);
-    // Facteur avec des coordonnées
-    public ICoord3D operator_mult(double b);
-    // Division entre deux coordonnées
-    public ICoord3D operator_div(ICoord3D b);
-    // Division avec des coordonnées
-    public ICoord3D operator_div(double b);
-    // Produit scalaire entre deux coordonnées
-    public double operator_scal(ICoord3D b);
-    // Produit vectoriel entre deux vecteur
-    public ICoord3D operator_produit_vect(ICoord3D b);
- 
+    // Ajout de matrices
+    public IMat operator_add(IMat b);
+    // Inversion de matrice
+    static public IMat operator_supp(IMat a);
+    // Difference entre deux matrices 
+    public IMat operator_supp(IMat b);
+    // Multiplication entre deux matrices
+    public IMat operator_mult(IMat b);
+    // Facteur avec une matrices
+    public IMat operator_mult(double b);
+    // Division entre deux matrices 
+    public IMat operator_div(IMat b);
+    // Division avec une matrices 
+    public IMat operator_div(double b);
 
 }
