74c86a1a35f351e472904805d1773435545c7ba2
machard <pascal.machard@gmail.com>
2024-02-20 15:34:15 +0100

    Update Coord3D.cs proportio

--- Coord3D.cs
+++ Coord3D.cs
@@ -54,7 +54,37 @@ class Coord3D : ICoord3d
     {
         return new Coord3D ( this.Y*b.Z - this.Z*b.Y, this.Z*b.X - this.X*b.Z, this.X*b.Y - this.Y*b.X );    
     }
-    
+
+    public bool IsProportional(other)
+    {
+        if (other.X == 0.0) && (X !== 0)
+            return false;
+        if (other.Y == 0.0) && (Y !== 0)
+            return false;
+        if (other.Z == 0.0) && (Z !== 0)
+            return false;
+            
+        double (other.X != 0.0) ? tx = X / other.X : 0.0;
+        double (other.Y != 0.0) ? ty = Y / other.Y : 0.0;
+        double (other.Z != 0.0) ? tz = Z / other.Z : 0.0;
+        if (tx == ty) && (ty == tz )) && ( tz == 0.0)
+            return true;
+        if (tx == 0.0)
+            return (ty == tz) ;
+        if (ty == 0.0)
+            return (tx == tz) ;
+        if (tz == 0.0)
+            return (tx == ty) ;
+        if (tx == ty) && (tx = 0.0)
+            return true;            
+        if (tx == tz) && (tz = 0.0)
+            return true;
+        if (ty == tz) && (tz = 0.0)
+            return true;
+        
+        return (tx == ty) && (tx == tz) ;
+    }
+
     public ICoord3D swap()
     {
         return new Coord3D( this.Z, this.Y, this.X);
