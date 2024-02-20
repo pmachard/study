39748f3cb1edaeb6a90899b9289aba3910d46ef1
machard <pascal.machard@gmail.com>
2024-02-20 15:13:38 +0100

    Update ILine3D.cs

--- ILine3D.cs
+++ ILine3D.cs
@@ -3,9 +3,9 @@ interface ILine3D
   public ICoord3D O {get; set};
   public ICoord3D V {get; set};
 
-  public bool IsParallel();
-  public bool IsPerpendicular();
-  public bool IsSecant();
+  public bool IsParallel(ILine3D other);
+  public bool IsPerpendicular(ILine3D other);
+  public bool IsSecant(ILine3D other);
 
   public ICoord3D Intersect(ILine3D other);
 }
