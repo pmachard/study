d5784331bcdc7d4c9cc7368377dc7b84cc858b28
machard <pascal.machard@gmail.com>
2024-02-20 15:19:55 +0100

    Update ICoord3D.cs

--- ICoord3D.cs
+++ ICoord3D.cs
@@ -44,7 +44,10 @@ interface ICoord3d
     public double operator_scal(ICoord3D b);
     // Produit vectoriel entre deux vecteur
     public ICoord3D operator_produit_vect(ICoord3D b);
-
+    
+    // vérifie que les deux coords sont propotionel
+    public bool IsProportional(other);
+    
     // Swap des valeurs (x=z,y=y,z=x) 
     public ICoord3D swap();
     
