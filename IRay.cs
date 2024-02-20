69f21951b2e3e20d6630ab2c0a88dc507358c804
machard <pascal.machard@gmail.com>
2024-02-20 15:01:04 +0100

    Update IRay.cs

--- IRay.cs
+++ IRay.cs
@@ -1,9 +1,14 @@
+// Rayon utilisé dans l'algo du raytracing
 interface IRay : IObject3D
 {
+  // constructeur par default du rayon
   public IRay();
+  // constructeur avec initialisation du rayon
   public IRay(ICoord3D o, ICoord3D v);
 
+  // origine du rayon
   public ICoord3D O { get; set; };
+  // vecteur directeur du rayon
   public ICoord3D V { get; set; };
 
 }
