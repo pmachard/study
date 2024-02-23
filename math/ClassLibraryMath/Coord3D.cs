43fad1d4efb906ffaadeea47eafd60ef4d5564f6
machard <pascal.machard@gmail.com>
2024-02-23 14:47:44 +0100

    refectoring interface icoord3d

---
+++ math/ClassLibraryMath/Coord3D.cs
@@ -0,0 +1,101 @@
+namespace math {
+
+
+    class Coord3D : ICoord3D
+    {
+        public Coord3D()
+        {
+            X = Y = Z = 0.0;
+        }
+
+        public Coord3D(double x, double y, double z)
+        {
+            X = x; Y = y; Z = z;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+
+        public ICoord3D operator_add(ICoord3D b)
+        {
+            return new Coord3D(this.X + b.X, this.Y + b.Y, this.Z + b.Z);
+        }
+
+        static public ICoord3D operator_supp(ICoord3D a)
+        {
+            return new Coord3D(this.X - b.X, this.Y - b.Y, this.Z - b.Z);
+        }
+
+        public ICoord3D operator_supp(ICoord3D b)
+        {
+            return new Coord3D(-b.X, -b.Y, -b.Z);
+        }
+
+        public ICoord3D operator_mult(ICoord3D b)
+        {
+            return new Coord3D(this.X * b.X, this.Y * b.Y, this.Z * b.Z);
+        }
+
+        public ICoord3D operator_mult(double b)
+        {
+            return new Coord3D(this.X * b, this.Y * b, this.Z * b);
+        }
+
+        public ICoord3D operator_div(ICoord3D b)
+        {
+            return new Coord3D(this.X / b.X, this.Y / b.Y, this.Z / b.Z);
+        }
+
+        public ICoord3D operator_div(double b)
+        {
+            return new Coord3D(this.X / b, this.Y / b, this.Z / b);
+        }
+
+        public double operator_scal(ICoord3D b)
+        {
+            return this.X / b.X + this.Y / b.Y + this.Z / b.Z;
+        }
+
+        public ICoord3d operator_produit_vect(ICoord3D b)
+        {
+            return new Coord3D(this.Y * b.Z - this.Z * b.Y, this.Z * b.X - this.X * b.Z, this.X * b.Y - this.Y * b.X);
+        }
+
+        public bool IsProportional(other)
+        {
+            if (other.X == 0.0) && (X !== 0)
+                return false;
+            if (other.Y == 0.0) && (Y !== 0)
+                return false;
+            if (other.Z == 0.0) && (Z !== 0)
+                return false;
+
+            double(other.X != 0.0) ? tx = X / other.X : 0.0;
+            double(other.Y != 0.0) ? ty = Y / other.Y : 0.0;
+            double(other.Z != 0.0) ? tz = Z / other.Z : 0.0;
+
+            if (tx == ty) && (ty == tz)) && (tz == 0.0)
+                return true;
+            if (tx == 0.0)
+                return (ty == tz);
+            if (ty == 0.0)
+                return (tx == tz);
+            if (tz == 0.0)
+                return (tx == ty);
+
+            return (tx == ty) && (tx == tz);
+        }
+
+        public ICoord3D swap()
+        {
+            return new Coord3D(this.Z, this.Y, this.X);
+        }
+
+        ICoord3D ICoord3d.operator_produit_vect(ICoord3D b)
+        {
+            throw new NotImplementedException();
+        }
+    };
+}
---
+++ math/ClassLibraryMath/ICoord3D.cs
@@ -0,0 +1,54 @@
+
+namespace math
+{
+    interface ICoord3D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        // Operateur addition entre deux coordonnées
+        static public ICoord3D operator +(ICoord3D a, ICoord3D b) { return a.operator_add(b); }
+        // Operateur d'inversion entre deux coordonnées
+        static public ICoord3D operator -(ICoord3D a) { return a.operator_supp(); }
+        // Operateur d'affection entre deux coordonnées
+        static public ICoord3D operator -(ICoord3D a, ICoord3D b) { return a.operator_supp(b); }
+        // Operateur de multiplication entre deux coordonnées
+        static public ICoord3D operator *(ICoord3D a, ICoord3D b) { return a.operator_mult(b); }
+        // Operateur de multiplication entre des coordonnées et un double
+        static public ICoord3D operator *(double m) { return a.operator_mult(m); }
+        // Operateur de division entre deux coordonnées
+        static public ICoord3D operator /(ICoord3D a, ICoord3D b) { return a.operator_div(b); }
+        // Operateur produit scalaire entre deux coordonnées
+        static public double operator ^(ICoord3D a, ICoord3D b) { return a.operator_scal(b); }
+        // Operator produit vectoriel entre deux coordonnées
+        static public ICoord3D operator &(ICoord3D a, ICoord3D b) { return a.operator_produit_vect(b); }
+
+        // Ajout de coodonnées
+        public ICoord3D operator_add(ICoord3D b);
+        // Inversion de coodonnées
+        public ICoord3D operator_supp();
+        // Difference entre deux coordonnées
+        public ICoord3D operator_supp(ICoord3D b);
+        // Multiplication entre deux coordonnées
+        public ICoord3D operator_mult(ICoord3D b);
+        // Facteur avec des coordonnées
+        public ICoord3D operator_mult(double b);
+        // Division entre deux coordonnées
+        public ICoord3D operator_div(ICoord3D b);
+        // Division avec des coordonnées
+        public ICoord3D operator_div(double b);
+        // Produit scalaire entre deux coordonnées
+        public double operator_scal(ICoord3D b);
+        // Produit vectoriel entre deux vecteur
+        public ICoord3D operator_produit_vect(ICoord3D b);
+
+        // vérifie que les deux coords sont propotionel
+        public bool IsProportional(ICoord3D other);
+
+        // Swap des valeurs (x=z,y=y,z=x) 
+        public ICoord3D swap();
+
+    }
+
+}
