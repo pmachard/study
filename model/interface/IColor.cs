6a50ddfa5f711c5dfaa867e89eeb9fe434296f79
machard <pascal.machard@gmail.com>
2024-04-16 18:49:20 +0200

    Update IColor.cs ajouter opérateur multiplie

--- model/interface/IColor.cs
+++ model/interface/IColor.cs
@@ -7,5 +7,9 @@ namespace Model.@interface
         public double R { get; set; }
         public double V { get; set; }
         public double B { get; set; }
+
+        // Operateur de multiplication avec une couleur 
+        static public ICoord3D operator *(ICoord3D a, double coef) { return a.operator_mult(coef); }
+        public IColor operator_mult(double coef);
     }
 }
